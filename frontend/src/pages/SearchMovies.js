2622ac056c6573a86bc6c602cfa68bac1b0ce4e9
Marina Kaeufer <marishutka@yahoo.com>
2022-07-15 18:34:02 -0400

    saving progress

--- frontend/src/pages/SearchMovies.js
+++ frontend/src/pages/SearchMovies.js
@@ -101,9 +101,35 @@ const SearchMovies = () => {
             ? `Viewing ${searchedMovies.length} results:`
             : 'Search for a movie to begin'}
         </h2>
+
+
+
+        <div class="card-deck">
+          
+          <div class="card">
+            <img class="card-img-top" src=".../100px180/" alt="Card image cap" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+          </div>
+          
+        </div>
+        
+
+
+
         <CardColumns>
           {searchedMovies.map((movie) => {
             return (
+
+
+              
+
+
               <Card key={movie.movieId} border='dark'>
                 {movie.image ? (
                   <Card.Img src={movie.image} alt={`The cover for ${movie.title}`} variant='top' />
